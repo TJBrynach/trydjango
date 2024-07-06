0fb2dc9257e0f6bd24a9d61d1a89a3f3f96ff5e0
TJBrynach <tjbshorney@gmail.com>
2024-07-06 19:46:41 +0100

    updates

--- src/pages/views.py
+++ src/pages/views.py
@@ -8,3 +8,21 @@ def home_view(request, *args, **kwargs):
     print(args, kwargs)
     #return HttpResponse("<h1>Hello World</h1>") # html code
     return render(request, "home.html",{})
+
+def contact_view(request, *args, **kwargs):
+    print(args, kwargs)
+    #return HttpResponse("<h1>Hello World</h1>") # html code
+    return render(request, "contact.html",{})
+
+def about_view(request, *args, **kwargs):
+    my_context = {
+        "my_text": "this is about me",
+        "my_number": 12345678,
+        "my_list": [1,23,45,678]
+    }
+    return render(request, "about.html", my_context)
+
+
+def social_view(request, *args, **kwargs):
+    print(args, kwargs)
+    return HttpResponse("<h1>Socials</h1>") # html code
