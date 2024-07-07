2762af05ecd29945404cf2aa4882680311df652b
TJBrynach <tjbshorney@gmail.com>
2024-07-07 21:36:35 +0100

    07_07_Update4

--- src/products/forms.py
+++ src/products/forms.py
@@ -3,6 +3,19 @@ from django import forms
 from .models import Product
 
 class ProductForm(forms.ModelForm):
+    title       = forms.CharField(label='', widget=forms.TextInput(attrs={"placeholder":"Title placeholder"}))
+    email       = forms.EmailField()
+    description = forms.CharField(  required=False, 
+                                    widget=forms.Textarea(
+                                            attrs={
+                                                "class": "new_class_name two",
+                                                "id": "my-id-for-textarea",
+                                                "rows": 10,
+                                                "columns": 5,
+                                                "placeholder":"description placeholder"
+                                            }
+                                    ))
+    price       = forms.DecimalField(initial=199.99)
     class Meta:
         model = Product
         fields = [
@@ -10,6 +23,20 @@ class ProductForm(forms.ModelForm):
             'description',
             'price'
         ]
+        
+    def clean_title(self, *args, **kwargs):
+        title = self.cleaned_data.get("title")
+        if not "Tom" in title:
+            raise forms.ValidationError("This is not a valid title")
+        return title
+    
+    
+    
+    def clean_email(self, *args, **kwargs):
+        email = self.cleaned_data.get("email")
+        
+        if not email.endswith(".edu"):
+            raise forms.ValidationError("This is not a valid email")
         
 class RawProductForm(forms.Form):
     title       = forms.CharField(label='', widget=forms.TextInput(attrs={"placeholder":"Title placeholder"}))
--- src/products/models.py
+++ src/products/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 # Create your models here.
-
 #https://docs.djangoproject.com/en/2.0/ref/models/fields/#field-types
 class Product(models.Model):
     title       = models.CharField(max_length=120) 
@@ -9,3 +8,7 @@ class Product(models.Model):
     price       = models.DecimalField(decimal_places=2, max_digits=10000)
     summary     = models.TextField(blank=False, null=False)
     featured    = models.BooleanField(default=True) 
+    
+    
+    def get_absolute_url(self):
+        return f"/products/{self.id}/"
